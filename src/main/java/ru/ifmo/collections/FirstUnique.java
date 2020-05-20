f98f77d88b2650ce0e230032f8c4eb7b4650588e
Egor Sergeenko <es.egor.sergeenko@gmail.com>
2020-05-20 21:28:01 +0300

    Add solution

--- src/main/java/ru/ifmo/collections/FirstUnique.java
+++ src/main/java/ru/ifmo/collections/FirstUnique.java
@@ -1,19 +1,38 @@
 package ru.ifmo.collections;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
 /**
  * Design a class which contains integers and returns first unique integer (in order of addition).
  * FirstUniqueTest can be used as an example.
  */
 public class FirstUnique {
+
+    private HashSet<Integer> elements = new HashSet<>();
+    private LinkedHashSet<Integer> uniqueElements = new LinkedHashSet<>();
+
     public FirstUnique(int[] numbers) {
-        // TODO implement
+        for (int value : numbers) {
+            add(value);
+        }
     }
 
     public int showFirstUnique() {
-        throw new UnsupportedOperationException(); // TODO implement
+        if (uniqueElements.isEmpty()) {
+            return -1;
+        } else {
+            return uniqueElements.iterator().next();
+        }
     }
 
     public void add(int value) {
-        throw new UnsupportedOperationException(); // TODO implement
+        if (!elements.contains(value)) {
+            elements.add(value);
+            uniqueElements.add(value);
+        } else {
+            elements.add(value);
+            uniqueElements.remove(value);
+        }
     }
 }
--- src/main/java/ru/ifmo/collections/KthLargest.java
+++ src/main/java/ru/ifmo/collections/KthLargest.java
@@ -1,18 +1,30 @@
 package ru.ifmo.collections;
 
+import java.util.Collections;
+import java.util.LinkedList;
+
+
 /**
  * Design a class to find the kth largest element in a stream. k is from 1 to numbers.length.
  * Note that it is the kth largest element in the sorted order, not the kth distinct element.
- *
+ * <p>
  * Constructor accepts an integer k and an integer array numbers, which contains initial elements from the stream.
  * For each call to the method KthLargest.add(), return the element representing the kth largest element in the stream.
  */
 public class KthLargest {
+    private final int K;
+    private LinkedList<Integer> elements = new LinkedList<>();
+
     public KthLargest(int k, int[] numbers) {
-        // TODO implement
+        this.K = k - 1;
+        for (int value : numbers) {
+            elements.add(value);
+        }
     }
 
     public int add(int val) {
-        throw new UnsupportedOperationException(); // TODO implement
+        elements.add(val);
+        elements.sort(Collections.reverseOrder());
+        return elements.get(K);
     }
 }
